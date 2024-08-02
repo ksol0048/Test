21bae858a941d2b2ab34101a42df68153e3a0b33
이재준 <ksol0048@naver.com>
2024-08-02 16:51:25 +0900

    수정 진행

--- src/main/java/kroryi/test/HelloApplication.java
+++ src/main/java/kroryi/test/HelloApplication.java
@@ -10,11 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        System.out.println("Git 시험");
+        System.out.println("Git 소스변경");
     }
 
     public static void main(String[] args) {
