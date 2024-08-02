53467cc9bccb555d5895a194433c3817d3954b20
이재준 <ksol0048@naver.com>
2024-08-02 16:24:23 +0900

    Study클래스 추가

--- src/main/java/kroryi/test/Study.java
+++ src/main/java/kroryi/test/Study.java
@@ -1,4 +1,17 @@
 package kroryi.test;
 
 public class Study {
+    String name;
+
+    public Study(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
